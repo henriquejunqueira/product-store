b3ccd2c1cd580a02f86768d0af7d29fd50fb7cab
Henrique Junqueira <henriquejunqueira15@gmail.com>
2024-05-16 23:59:03 -0300

    feita primeira estilização do component header

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent {}
 
-}
+// ! aula 7 link:
+// ? https://www.youtube.com/watch?v=MK1FLZn3Jbc&list=PLXEUJjGpEX7zwdFSAzIPiSf9p0tOeI1Yu&index=7
