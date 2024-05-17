18d3c6c669e84a5f2035e546d70371b0600b75e6
Henrique Junqueira <henriquejunqueira15@gmail.com>
2024-05-17 15:31:59 -0300

    criado card de produto, lista e botão novo produto

---
+++ src/app/features/list/list.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { ProductsService } from '../../shared/services/products.service';
+import { Product } from '../../shared/interfaces/product.interface';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-list',
+  standalone: true,
+  imports: [CardComponent, RouterLink, MatButtonModule],
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.scss',
+})
+export class ListComponent {
+  products: Product[] = [];
+
+  productsService = inject(ProductsService);
+
+  ngOnInit() {
+    this.productsService.getAll().subscribe((products) => {
+      this.products = products;
+    });
+  }
+}
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -9,6 +9,3 @@ import { MatToolbarModule } from '@angular/material/toolbar';
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {}
-
-// ! aula 7 link:
-// ? https://www.youtube.com/watch?v=MK1FLZn3Jbc&list=PLXEUJjGpEX7zwdFSAzIPiSf9p0tOeI1Yu&index=7
